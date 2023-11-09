5c73d4a793058eda3bd3fcee5edd0cd044861059
Angelo Brown <130711987+angelob120@users.noreply.github.com>
2023-11-09 15:18:19 -0500

    fixed small change

--- script.js
+++ script.js
@@ -21,3 +21,31 @@ document.getElementById('contact-form').addEventListener('submit', function(even
             submitBtn.value = 'Send Email';
         });
 });
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Get the buttons by their class name
+    const bookCallButton = document.querySelector('.book-call');
+    const myWorkButton = document.querySelector('.my-work');
+
+    // Function to scroll to a section
+    function scrollToSection(selector) {
+        const section = document.querySelector(selector);
+        if(section) {
+            section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    }
+
+    // Event listener for 'Book a Call' button
+    bookCallButton.addEventListener('click', () => {
+        scrollToSection('#book-call-section');
+    });
+
+    // Event listener for 'My Work' button
+    myWorkButton.addEventListener('click', () => {
+        scrollToSection('#my-work-section');
+    });
+});
